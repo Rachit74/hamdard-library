86fb54863d04b1098f9e56d1ed33d3f92c5a420e
Rachit74 <rachithooda09@gmail.com>
2024-07-21 13:31:19 +0530

    file upload eroor

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
-# from . import db
+from . import db
 
-# class File(db.Model):
-#     id = db.Column(db.Integer(),primary_key=True)
-#     file_name = db.Column(db.String(200))
-#     dept = db.Column(db.String(100))
+class File(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    file_name = db.Column(db.String(200))
+    file_path = db.Column(db.String(50))
+    dept = db.Column(db.String(100))
--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, send_from_directory, redirect,url_for
+from werkzeug.utils import secure_filename
+import os
+from . import UPLOAD_FOLDER
+from . import db
+from .models import File
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +11,20 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('index.html')
 
-@views.route('/upload')
+@views.route('/upload', methods=['GET','POST'])
 def upload():
-    return render_template("upload.html")
+    if request.method == "POST":
+        file = request.files['file']
+        title = request.form['title']
+        dept = request.form['dept']
+
+        if file and title and dept:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join('./static', 'uploads')
+            file.save(file_path)
+            
+            new_file = File(file_name=title, file_path=filename, dept=dept)
+            db.session.add(new_file)
+            db.session.commit()
+            return redirect(url_for('index'))
+    return render_template('upload.html')
