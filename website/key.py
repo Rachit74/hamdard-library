312f459438f905833fa33c6ee0ed3daf2cf91419
Rachit74 <rachithooda09@gmail.com>
2024-08-08 07:58:26 +0530

    require

--- website/key.py
+++ website/key.py
@@ -3,6 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 KEY_FILE = os.path.join(BASE_DIR, 'key.txt')
 
+print(f"Looking for key file at: {KEY_FILE}")
+
 with open(KEY_FILE, 'r') as file:
-    global key
     key = file.read()
