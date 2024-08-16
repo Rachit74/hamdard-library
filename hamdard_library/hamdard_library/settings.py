ef44cea1f70c514edffc4a41617cf8674e093dd3
Rachit74 <rachithooda09@gmail.com>
2024-08-16 19:08:29 +0530

    render Test deploy

--- hamdard_library/hamdard_library/settings.py
+++ hamdard_library/hamdard_library/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hthl$k(=-vtx)c(i17zl56yysa6^*^$k=&0=%ko-d%9odf0hwd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
