55f1c2beba287ecbba7c1565dd478f79c9f45527
Rachit74 <rachithooda09@gmail.com>
2024-08-17 10:20:54 +0530

    db reset and fix ignore

--- hamdard_library/hamdard_library/settings.py
+++ hamdard_library/hamdard_library/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hthl$k(=-vtx)c(i17zl56yysa6^*^$k=&0=%ko-d%9odf0hwd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
