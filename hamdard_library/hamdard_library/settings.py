2fc073b2fdc99ac4323b6ea3ec7b548ed253838c
Rachit74 <rachithooda09@gmail.com>
2024-08-18 16:33:09 +0530

    some touches

--- hamdard_library/hamdard_library/settings.py
+++ hamdard_library/hamdard_library/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'hamdard_library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DJANGO_DB_NAME'),
-        'USER': os.getenv('DJANGO_DB_USER'),
-        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
-        'HOST': os.getenv('DJANGO_DB_HOST'),
-        'PORT': os.getenv('DJANGO_DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
